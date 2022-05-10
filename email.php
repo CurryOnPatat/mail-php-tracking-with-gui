b9354dbe547853a0fa2f746e82927caa1b210a26
CurryOnPatat <99186666+CurryOnPatat@users.noreply.github.com>
2022-05-10 15:46:08 +0200

    v1.0.1
    REmoved a folder and move the files

--- email.php
+++ email.php
@@ -8,7 +8,7 @@
         </style>
 <div class="container column">
 <h1> your link that you need to put in the email is: 
-<?php echo htmlspecialchars('<img src="https://emailfooter.lauwielocal.tk/footer.php?image=footer.gif&id="'.$_GET["email"].'">'); ?>
+<?php echo htmlspecialchars('<img src="https://yoururlgoeshere/footer.php?image=footer.gif&id="'.$_GET["email"].'">'); ?>
 </h1>
 </div>
 
--- link.php
+++ link.php
@@ -6,7 +6,7 @@
 
 <div class="container column">
 <h1><form action="email.php" method="get">
-Email adress van die persoon: <input type="text" name="email"><br>
+Email address from the person: <input type="text" name="email"><br>
 <input class="button"type="submit"></h1>
 
 
